b3f7ca8044655c3b8ea33ed911e916aec72c8e7f
Srinivas Elimilla <114722049+elimillasrinivas@users.noreply.github.com>
2023-04-18 11:33:08 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,9 +14,9 @@ const Header = () => {
         <div className="links">
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="https://srinivaselimilla.netlify.app/about">About</a></li>
+                <li><a href="https://srinivaselimilla.netlify.app/projects">Projects</a></li>
+                <li><a href="https://srinivaselimilla.netlify.app/contact">Contact</a></li>
             </ul>
         </div>
         <div className="resume">
