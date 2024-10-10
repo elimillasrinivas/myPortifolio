1f54f35a5c10e8c79f6f8548dd02f74553746525
srinivas-elimilla <srinivaselimilla1@gmail.com>
2024-10-11 00:15:46 +0530

    added jobminar project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -20,20 +20,48 @@ import p41 from "../../assets/p4-1.png";
 import p42 from "../../assets/p4-2.png";
 import p43 from "../../assets/p4-3.png";
 import p51 from "../../assets/p5-1.png";
-
+import p61 from "../../assets/p6-1.png";
+import p62 from "../../assets/p6-2.png";
+import p63 from "../../assets/p6-3.png";
+import p64 from "../../assets/p6-4.png";
+import p65 from "../../assets/p6-5.png";
 
 const Projects = () => {
   const images1 = [p11, p12, p13, p14, p15, p16, p17];
   const images2 = [p21, p22, p23];
-  const images3 = [p31, p32, p33,p34];
+  const images3 = [p31, p32, p33, p34];
   const images4 = [p41, p42, p43];
+  const images5 = [p61, p62, p63, p64, p65];
 
   return (
     <div className="projects-container">
       <div className="card">
         <SimpleImageSlider
-          width={1150}
-          height={650}
+          width="89.9%"
+          height="90vh"
+          images={images5}
+          showBullets={true}
+          showNavs={true}
+        />
+        <h1>Jobminar Pvt Ltd</h1>
+        <p>
+          This MERN stack web app facilitates efficient communication between
+          users and an admin. Users can submit contact or business inquiries
+          through forms, which are then sent to the admin's email via
+          Nodemailer. JWT authentication ensures secure access to the admin
+          panel.
+        </p>
+        <button>
+          <a href="https://www.jobminar.com/" target="_blank">
+            See Live{" "}
+          </a>
+        </button>
+      </div>
+
+      <div className="card">
+        <SimpleImageSlider
+          width="89.9%"
+          height="90vh"
           images={images1}
           showBullets={true}
           showNavs={true}
@@ -52,52 +80,88 @@ const Projects = () => {
           browse through the proposals and select one that meets their needs.
           Customers will be allowed to select only one proposal per event.
         </p>
-        <button><a href="https://eventproposalpage-project.netlify.app/" target="_blank">See Live </a></button>
+        <button>
+          <a
+            href="https://eventproposalpage-project.netlify.app/"
+            target="_blank"
+          >
+            See Live{" "}
+          </a>
+        </button>
       </div>
 
       <div className="card">
-      <SimpleImageSlider
-          width={1150}
-          height={650}
+        <SimpleImageSlider
+          width="89.9%"
+          height="90vh"
           images={images2}
           showBullets={true}
           showNavs={true}
-
         />
         <h1>ToDo List</h1>
         <p>
-        A todo list is a simple but effective tool used for organizing and managing tasks and activities. It typically consists of a list of items or tasks that need to be completed. The purpose of a todo list is to help the user keep track of what needs to be done, prioritize tasks, and stay focused on their goals. By breaking down larger projects into smaller, manageable tasks, a todo list can also help reduce stress and increase productivity.
+          A todo list is a simple but effective tool used for organizing and
+          managing tasks and activities. It typically consists of a list of
+          items or tasks that need to be completed. The purpose of a todo list
+          is to help the user keep track of what needs to be done, prioritize
+          tasks, and stay focused on their goals. By breaking down larger
+          projects into smaller, manageable tasks, a todo list can also help
+          reduce stress and increase productivity.
         </p>
-        <button><a href="https://elimillasrinivas.github.io/Todo-List-App/" target="_blank">See Live </a></button>
+        <button>
+          <a
+            href="https://elimillasrinivas.github.io/Todo-List-App/"
+            target="_blank"
+          >
+            See Live{" "}
+          </a>
+        </button>
       </div>
 
       <div className="card">
-      <SimpleImageSlider
-          width={1150}
-          height={650}
+        <SimpleImageSlider
+          width="89.9%"
+          height="90vh"
           images={images4}
           showBullets={true}
           showNavs={true}
-
         />
         <h1>Calculator</h1>
         <p>
-        A calculator built using HTML, CSS, and JavaScript is a simple yet effective project.
-        The calculator designed using HTML and CSS to create the user interface. The interface typically includes a display area for showing the input and output, as well as buttons for numbers, operators, and functions. CSS can be used to style the interface and make it visually appealing.The calculator's functionality can be implemented using JavaScript.
+          A calculator built using HTML, CSS, and JavaScript is a simple yet
+          effective project. The calculator designed using HTML and CSS to
+          create the user interface. The interface typically includes a display
+          area for showing the input and output, as well as buttons for numbers,
+          operators, and functions. CSS can be used to style the interface and
+          make it visually appealing.The calculator's functionality can be
+          implemented using JavaScript.
         </p>
-        <button><a href="https://elimillasrinivas.github.io/Calculator/" target="_blank">See Live </a></button>
+        <button>
+          <a
+            href="https://elimillasrinivas.github.io/Calculator/"
+            target="_blank"
+          >
+            See Live{" "}
+          </a>
+        </button>
       </div>
       <div className="card">
-        <img
-          src={p51}
-          alt="sde"
-        />
+        <img src={p51} alt="sde" />
         <h1>Digital Clock</h1>
         <p>
-        A Digital Clock built using HTML, CSS, and JavaScript is a simple yet effective project.
-        The Digital Clock designed using HTML and CSS to create the user interface. The interface typically includes a display area for showing the live time.
+          A Digital Clock built using HTML, CSS, and JavaScript is a simple yet
+          effective project. The Digital Clock designed using HTML and CSS to
+          create the user interface. The interface typically includes a display
+          area for showing the live time.
         </p>
-        <button><a href="https://elimillasrinivas.github.io/digitalclock/" target="_blank">See Live </a></button>
+        <button>
+          <a
+            href="https://elimillasrinivas.github.io/digitalclock/"
+            target="_blank"
+          >
+            See Live{" "}
+          </a>
+        </button>
       </div>
       <div className="card">
         <img
